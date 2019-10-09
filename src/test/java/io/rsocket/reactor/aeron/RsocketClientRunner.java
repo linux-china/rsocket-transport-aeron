279e46aec623c86cf29c26bc50ca74def80d53c2
linux_china <libing.chen@gmail.com>
2019-10-08 18:20:35 -0700

    adjust to use uri registry

--- src/test/java/io/rsocket/reactor/aeron/RsocketClientRunner.java
+++ src/test/java/io/rsocket/reactor/aeron/RsocketClientRunner.java
@@ -1,49 +1,32 @@
 package io.rsocket.reactor.aeron;
 
 import io.rsocket.Payload;
+import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
+import io.rsocket.uri.UriTransportRegistry;
 import io.rsocket.util.ByteBufPayload;
-import reactor.aeron.AeronClient;
-import reactor.aeron.AeronResources;
 import reactor.core.publisher.Flux;
 
 public class RsocketClientRunner {
 
-  public static void main(String[] args) throws Exception {
-
-    AeronResources aeronResources = new AeronResources().useTmpDir().singleWorker().start().block();
-    try {
-
-      // start client
-      RSocketFactory.connect()
-          .transport(
-              () ->
-                  new AeronClientTransport(
-                      AeronClient.create(aeronResources).options("localhost", 12000, 12001)))
-          .start()
-          .log("client connect() ")
-          .subscribe(
-              rSocket -> {
-                System.err.println("start " + rSocket);
-
-                Flux.range(0, 1000)
-                    .flatMap(
-                        i ->
-                            rSocket
-                                .requestResponse(ByteBufPayload.create("Hello_" + i))
+    public static void main(String[] args) throws Exception {
+        final RSocket rSocket = RSocketFactory.connect()
+                .transport(UriTransportRegistry.clientForUri("aeron://localhost:12000"))
+                .start()
+                .block();
+        System.err.println("start " + rSocket);
+        Flux.range(0, 10)
+                .flatMap(i ->
+                        rSocket.requestResponse(ByteBufPayload.create("Hello_" + i))
                                 .log("receive ")
                                 .map(Payload::getDataUtf8)
                                 .doOnNext(System.out::println)
                                 .then())
-                    .doFinally(s -> rSocket.dispose())
-                    .subscribe();
-              });
-
-      System.err.println("wait for the end");
-      Thread.currentThread().join();
-    } finally {
-      aeronResources.dispose();
-      aeronResources.onDispose().block();
+                .subscribe();
+        System.err.println("wait for the end");
+        Thread.sleep(2000);
+        rSocket.dispose();
+        rSocket.onClose().block();
+        System.exit(0);
     }
-  }
 }
--- src/test/java/io/rsocket/reactor/aeron/RsocketServerRunner.java
+++ src/test/java/io/rsocket/reactor/aeron/RsocketServerRunner.java
@@ -3,75 +3,65 @@ package io.rsocket.reactor.aeron;
 import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.RSocketFactory;
+import io.rsocket.uri.UriTransportRegistry;
 import io.rsocket.util.ByteBufPayload;
-import java.time.Duration;
 import org.reactivestreams.Publisher;
-import reactor.aeron.AeronResources;
-import reactor.aeron.AeronServer;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 public class RsocketServerRunner {
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        // start server
+        RSocketFactory.receive()
+                .acceptor(
+                        (setup, reactiveSocket) ->
+                                Mono.just(
+                                        new AbstractRSocket() {
 
-    AeronResources aeronResources = new AeronResources().useTmpDir().singleWorker().start().block();
-    try {
-      // start server
-      RSocketFactory.receive()
-          .acceptor(
-              (setup, reactiveSocket) ->
-                  Mono.just(
-                      new AbstractRSocket() {
+                                            @Override
+                                            public Mono<Void> fireAndForget(Payload payload) {
+                                                System.err.println(
+                                                        "fireAndForget(), receive request: " + payload.getDataUtf8());
+                                                return super.fireAndForget(payload);
+                                            }
 
-                        @Override
-                        public Mono<Void> fireAndForget(Payload payload) {
-                          System.err.println(
-                              "fireAndForget(), receive request: " + payload.getDataUtf8());
-                          return super.fireAndForget(payload);
-                        }
+                                            @Override
+                                            public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
+                                                System.err.println("requestChannel(), receive request: ");
+                                                return super.requestChannel(payloads);
+                                            }
 
-                        @Override
-                        public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
-                          System.err.println("requestChannel(), receive request: ");
-                          return super.requestChannel(payloads);
-                        }
+                                            @Override
+                                            public Mono<Void> metadataPush(Payload payload) {
+                                                System.err.println(
+                                                        "metadataPush(), receive request: " + payload.getDataUtf8());
+                                                return super.metadataPush(payload);
+                                            }
 
-                        @Override
-                        public Mono<Void> metadataPush(Payload payload) {
-                          System.err.println(
-                              "metadataPush(), receive request: " + payload.getDataUtf8());
-                          return super.metadataPush(payload);
-                        }
+                                            @Override
+                                            public Mono<Payload> requestResponse(Payload payload) {
+                                                System.err.println(
+                                                        "requestResponse(), receive request: " + payload.getDataUtf8());
+                                                return Mono.just(payload);
+                                            }
 
-                        @Override
-                        public Mono<Payload> requestResponse(Payload payload) {
-                          System.err.println(
-                              "requestResponse(), receive request: " + payload.getDataUtf8());
-                          return Mono.just(payload);
-                        }
+                                            @Override
+                                            public Flux<Payload> requestStream(Payload payload) {
+                                                System.err.println(
+                                                        "requestStream(), receive request: " + payload.getDataUtf8());
+                                                return Flux.interval(Duration.ofMillis(100))
+                                                        .log("send back ")
+                                                        .map(aLong -> ByteBufPayload.create("Interval: " + aLong));
+                                            }
+                                        }))
+                .transport(UriTransportRegistry.serverForUri("aeron://localhost:12000"))
+                .start()
+                .subscribe();
 
-                        @Override
-                        public Flux<Payload> requestStream(Payload payload) {
-                          System.err.println(
-                              "requestStream(), receive request: " + payload.getDataUtf8());
-                          return Flux.interval(Duration.ofMillis(100))
-                              .log("send back ")
-                              .map(aLong -> ByteBufPayload.create("Interval: " + aLong));
-                        }
-                      }))
-          .transport(
-              () ->
-                  new AeronServerTransport(
-                      AeronServer.create(aeronResources).options("localhost", 12000, 12001)))
-          .start()
-          .subscribe();
-
-      System.err.println("wait for the end");
-      Thread.currentThread().join();
-    } finally {
-      aeronResources.dispose();
-      aeronResources.onDispose().block();
+        System.err.println("wait for the end");
+        Thread.currentThread().join();
     }
-  }
 }
